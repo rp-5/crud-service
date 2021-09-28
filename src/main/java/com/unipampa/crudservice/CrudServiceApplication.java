31d3ef0fc56bc4ae97f8ff8f21f656c5ebec5fe8
Lucas <47724710+fell-lucas@users.noreply.github.com>
2021-09-28 15:44:30 -0300

    add deploy database and remove eureka (#28)

--- src/main/java/com/unipampa/crudservice/CrudServiceApplication.java
+++ src/main/java/com/unipampa/crudservice/CrudServiceApplication.java
@@ -2,10 +2,8 @@ package com.unipampa.crudservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class CrudServiceApplication {
 
 	public static void main(String[] args) {

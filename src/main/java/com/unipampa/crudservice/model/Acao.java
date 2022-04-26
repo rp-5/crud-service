4f825c12ff58ccd1fce3917545a8ac106834827a
Samuel <samuelmodestoes@gmail.com>
2022-04-25 23:02:03 -0300

    implementacao do spring validation em Acao

--- src/main/java/com/unipampa/crudservice/model/Acao.java
+++ src/main/java/com/unipampa/crudservice/model/Acao.java
@@ -1,21 +1,19 @@
 package com.unipampa.crudservice.model;
 
-import java.util.Set;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
 
 import javax.persistence.*;
-
-import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.*;
+import java.util.UUID;
 
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Entity
-@Table(name = "tb_acao")
 public class Acao {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private UUID id;
 
   @Column(nullable = false, unique = true, length = 50)
   private String nome;

51bfe7c3034b0a3eed7651392487ea28f858e6ad
Samuel <samuelmodestoes@gmail.com>
2022-04-29 20:32:08 -0300

    implementacao das validacoes por anotacao

--- src/main/java/com/unipampa/crudservice/dto/AmostraDTO.java
+++ src/main/java/com/unipampa/crudservice/dto/AmostraDTO.java
@@ -5,7 +5,9 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.NotFound;
 
+import javax.validation.constraints.NotBlank;
 import java.util.Date;
 import java.util.List;
 
@@ -15,15 +17,32 @@ import java.util.List;
 @NoArgsConstructor
 public class AmostraDTO {
 
-
     private Long numero;
+
+    @NotBlank
     private Boolean lvc;
+
+    @NotBlank
     private Boolean morreu;
+
+    @NotBlank
     private List<Cao> caes;
+
+    @NotBlank
     private Proprietario proprietario;
+
+    @NotBlank
     private List<Localizacao> localizacoes;
+
+    @NotBlank
     private List<Sintoma> sintomas;
+
+    @NotBlank
     private List<Exame> exames;
+
+    @NotBlank
     private Acao acao;
+
+    @NotBlank
     private Amostra amostra;
 }
